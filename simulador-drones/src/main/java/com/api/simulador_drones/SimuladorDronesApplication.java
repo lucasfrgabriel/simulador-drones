ce475644baf10183982bab19666d9d038da54875
lucasfrgabriel <lucasfrgabriel@gmail.com>
2025-09-27 14:17:07 -0300

    project created

---
+++ simulador-drones/src/main/java/com/api/simulador_drones/SimuladorDronesApplication.java
@@ -0,0 +1,13 @@
+package com.api.simulador_drones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorDronesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorDronesApplication.class, args);
+	}
+
+}
---
+++ simulador-drones/src/test/java/com/api/simulador_drones/SimuladorDronesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.simulador_drones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorDronesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
